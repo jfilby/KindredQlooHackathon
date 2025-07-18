a50573e492e4ae08f96992808b40ca2e2249c7c2
jfilby <jason.filby@gmail.com>
2025-07-18 18:48:43 +0200

    Move the standard LLM variant name to use to the env file.

--- src/nextjs/server/src/services/llms/get-tech-service.ts
+++ src/nextjs/server/src/services/llms/get-tech-service.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { CustomError } from '@/serene-core-server/types/errors'
 import { TechModel } from '@/serene-core-server/models/tech/tech-model'
-import { AiTechDefs } from '@/serene-ai-server/types/tech-defs'
 
 // Models
 const techModel = new TechModel()
@@ -16,11 +16,24 @@ export class GetTechService {
           prisma: PrismaClient,
           userProfileId: string | null) {
 
+    // Debug
+    const fnName = `${this.clName}.getStandardLlmTech()`
+
+    // Defined LLM variant name
+    const variantName = process.env.STANDARD_LLM_VARIANT_NAME!
+
     // Get the standard LLM to use
     const tech = await
             techModel.getByVariantName(
               prisma,
-              AiTechDefs.googleGemini_V2pt5FlashFree)
+              variantName)
+
+    // Validate
+    if (tech == null) {
+      throw new CustomError(`${fnName}: tech == null for variantName: ` +
+                            `${variantName}`
+      )
+    }
 
     // Return
     return tech
