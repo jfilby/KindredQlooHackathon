9897322c5838165d0c59146f1c793ae096d374bd
jfilby <jason.filby@gmail.com>
2025-07-20 17:22:57 +0200

    Update the server production script.

--- src/nextjs/server/scripts/production.sh
+++ src/nextjs/server/scripts/production.sh
@@ -1,6 +1,6 @@
 export NODE_ENV=production
 export DATABASE_URL=postgresql://kindred:kindred@localhost/kindred?host=/var/run/postgresql/
 
-export GRAPHQL_URL=http://localhost:4000
-export GRAPHQL_URL_FULL=http://localhost:4000/graphql
+export GRAPHQL_URL=http://localhost:4020
+export GRAPHQL_URL_FULL=http://localhost:4020/graphql
 
