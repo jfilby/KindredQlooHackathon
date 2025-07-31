bbb51901001453c6ab351509aa644a747a5cf887
jfilby <jason.filby@gmail.com>
2025-07-31 17:28:07 +0200

    Added favicon and background image to the about page.

--- src/nextjs/client/src/pages/about.tsx
+++ src/nextjs/client/src/pages/about.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Image } from 'mui-image'
 import { Typography } from '@mui/material'
 import { loadServerPage } from '@/services/page/load-server-page'
 import Layout from '@/components/layouts/layout'
@@ -24,34 +25,54 @@ export default function AboutPage({
           About
         </Typography>
 
-        <Typography
-          style={{ textAlign: 'left' }}
-          variant='body1'>
-            
-          Kindred summaries HackerNews posts and their related information. These
-          summaries are then displayed on a web page, make it fast and easy to stay
-          up-to-date with the latest HN posts.
-          <br/><br/>
+        <div style={{ position: 'relative', height: '90vh', width: '100%' }}>
 
-          Each post summary includes links to both the story's URL if present and the
-          original post on HN. A list of key insights is presented per post. Clicking an
-          insight shows its most relevant comments in-line, each clickable to view the
-          original on HN.
-          <br/><br/>
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundImage: 'url("http://localhost:3001/kindred-logo.jpg")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            opacity: 0.1,
+            zIndex: 0,
+          }}/>
 
-          You can also chat with Kindred about a post summary to ask any questions you
-          may have about the story, the post and its comments.
-          <br/>
+          <div style={{ position: 'relative', zIndex: 1, color: 'black', padding: '2rem' }}>
+            <Typography
+              style={{ color: 'black', textAlign: 'left' }}
+              variant='body1'>
 
-          Easily specify your interests and tastes, which can be related to or
-          independent of the social media site summarized. The system uses this
-          information to rerank the post summaries according to each user's interests.
-          <br/><br/>
+            Kindred summaries HackerNews posts and their related information. These
+            summaries are then displayed on a web page, make it fast and easy to stay
+            up-to-date with the latest HN posts.
+            <br/><br/>
 
-          Tastes include books, movies, podcasts and more. Recommendedations for tastes
-          are also sourced once any tastes are initially added, via Qloo's insights API.
-        </Typography>
-      </Layout>
+            Each post summary includes links to both the story's URL if present and the
+            original post on HN. A list of key insights is presented per post. Clicking an
+            insight shows its most relevant comments in-line, each clickable to view the
+            original on HN.
+            <br/><br/>
+
+            You can also chat with Kindred about a post summary to ask any questions you
+            may have about the story, the post and its comments.
+            <br/><br/>
+
+            Easily specify your interests and tastes, which can be related to or
+            independent of the social media site summarized. The system uses this
+            information to rerank the post summaries according to each user's interests.
+            <br/><br/>
+
+            Tastes include books, movies, podcasts and more. Recommendedations for tastes
+            are also sourced once any tastes are initially added, via Qloo's insights API.
+          </Typography>
+          </div>
+        </div>
+
+     </Layout>
     </>
   )
 }
